9c7e17af3e2bb1a4075923449343c0d5c4c5a254
Suraj <suraj4311899@gmail.com>
2026-05-30 00:23:37 +0545

    Fix production build

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -5,9 +5,7 @@ import {
   FaXTwitter,
 } from "react-icons/fa6";
 import "./styles/SocialIcons.css";
-import { TbNotes } from "react-icons/tb";
 import { useEffect } from "react";
-import HoverLinks from "./HoverLinks";
 
 const SocialIcons = () => {
   useEffect(() => {
